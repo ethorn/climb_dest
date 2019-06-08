ce45a13d2649ade0c237883be29b80f8894214ee
Eric Thörn <eric.m.thorn@gmail.com>
2019-06-08 20:57:35 +0200

    Fixed uploads url.

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADS_DEFAULT_DEST = 'app/static/uploads'
-    UPLOADS_DEFAULT_URL = 'http://127.0.0.1:5000/static/uploads/'
+    UPLOADS_DEFAULT_URL = os.environ.get('UPLOADS_URL') or 'http://127.0.0.1:5000/static/uploads/'
     UPLOADED_IMAGES_DEST = 'app/static/uploads/images'
-    UPLOADED_IMAGES_URL = 'http://127.0.0.1:5000/static/uploads/images/'
+    UPLOADED_IMAGES_URL = os.environ.get('UPLOADED_IMG_URL') or 'http://127.0.0.1:5000/static/uploads/images/'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
