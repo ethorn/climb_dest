9f6a4df3fab069a6760a06f5db448cf95693d5c1
Eric Thörn <eric.m.thorn@gmail.com>
2019-09-28 11:50:42 +0200

    cleanup: adding a deployment folder to the project

--- app/__init__.py
+++ app/__init__.py
@@ -46,7 +46,7 @@ if not app.debug:
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/climbing_destinations.log', maxBytes=10240,
+    file_handler = RotatingFileHandler('logs/climb_dest.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
